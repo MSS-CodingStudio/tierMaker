7bc72508e8409bd257ecd338e444ae283112edc0
YuKannagi <shenyoumu00@gmail.com>
2023-06-04 17:37:48 +0900

    トップページ

--- tiermaker/lib/main.dart
+++ tiermaker/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,109 +9,170 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'homePage',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State with SingleTickerProviderStateMixin {
+  // ページ切替え用コントローラ
+  late PageController _pageController;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // ページインデックス
+  int _screen = 0;
+
+  // タイトル
+  var _title = '表一覧';
+
+  // 右上ボタンの表示名
+  var _buttonLabel = '新規作成';
+
+  @override
+  void initState() {
+    super.initState();
+    // コントローラ作成
+    _pageController = PageController(initialPage: _screen);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // コントローラ破棄
+    _pageController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: const Color(0xFF272727),
+      // ヘッダ
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        backgroundColor: const Color(0xFF272727),
+        elevation: 0,
+        title: Text(_title),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.help_outline,
+            color: Color(0xFF00A3FF),
+            size: 30,
+          ),
         ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 20),
+            ),
+            child: Text(_buttonLabel),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // ページ
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _screen = index;
+          });
+        },
+        children: const [
+          tierList(),
+          imageFolder(),
+        ],
+      ),
+      // フッタ
+      bottomNavigationBar: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          BottomNavigationBar(
+            selectedItemColor: const Color(0xFFFFFFFF),
+            unselectedItemColor: const Color(0xFF555555),
+            backgroundColor: const Color(0xFF272727),
+            currentIndex: _screen,
+            onTap: (index) {
+              setState(() {
+                _screen = index;
+                if (_screen == 0) {
+                  _title = '表一覧';
+                  _buttonLabel = '新規追加';
+                } else {
+                  _title = '画像フォルダ';
+                  _buttonLabel = '作成';
+                }
+                _pageController.animateToPage(
+                  index,
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeOut,
+                );
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list_alt),
+                label: '表一覧',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.folder),
+                label: '画像フォルダ',
+              ),
+            ],
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            child: Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  gradient: LinearGradient(
+                    begin: FractionalOffset.bottomLeft,
+                    end: FractionalOffset.topRight,
+                    colors: [Color(0xFFCB0E52), Color(0xFFE80F91)],
+                  )),
+              child: const Icon(Icons.add),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// 表一覧
+class tierList extends StatelessWidget {
+  const tierList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF272727),
+      body: Center(child: Icon(Icons.list_alt)),
+    );
+  }
+}
+
+// 画像フォルダ
+class imageFolder extends StatelessWidget {
+  const imageFolder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF272727),
+      body: Center(child: Icon(Icons.folder)),
     );
   }
 }
